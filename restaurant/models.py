05853117dbff52815548b30cd61401937e04bed5
Telecowmoonication <mikaylapastoor@hotmail.com>
2024-04-19 21:45:23 -0700

    Added basic Booking and Menu models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(default=1)
+    booking_date = models.DateTimeField()
+    
+    def __str__(self)-> str:
+        return f"Reservation by {self.name} for {self.no_of_guests}"
+    
+    
+class Menu(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, db_index=True)
+    inventory = models.IntegerField(default=0)
+    
+    def __str__(self)-> str:
+        return self.title
+    
