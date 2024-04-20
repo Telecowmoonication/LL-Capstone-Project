b923aac233a0b609e8f5ff74df9f7f1d48241ccd
Telecowmoonication <mikaylapastoor@hotmail.com>
2024-04-20 13:01:54 -0700

    Setting up menu API

--- restaurant/models.py
+++ restaurant/models.py
@@ -11,7 +11,7 @@ class Booking(models.Model):
         return f"Reservation by {self.name} for {self.no_of_guests}"
     
     
-class Menu(models.Model):
+class MenuItem(models.Model):
     title = models.CharField(max_length=255, db_index=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, db_index=True)
     inventory = models.IntegerField(default=0)
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Booking, MenuItem
+from .serializers import BookingSerializer, MenuItemSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+class bookingView(APIView):
+    def get(self, request):
+        items = Booking.objects.all()
+        serializer = BookingSerializer(items, many=True)
+        return Response(serializer.data)
+    
+
+## This was in the course instructions at one point but then seems to get switched out for menuitemsview
+# class menuview(APIView):
+#     def post(self, request):
+#         serializer = MenuItemSerializer(data=request.data)
+        
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({"status": "Success", "data": serializer.data})
+        
+class menuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    
+class singleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
