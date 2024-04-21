54c6b79f06ead2339f6bd709d584aec090ff16ec
Telecowmoonication <mikaylapastoor@hotmail.com>
2024-04-21 13:00:19 -0700

    Added some authentication and checked registration

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
-from .views import index, menuItemsView, singleMenuItemView, bookingViewSet
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import index, msg,menuItemsView, singleMenuItemView, bookingViewSet
 
 urlpatterns = [
     path('', index, name='index'), # Endpoint: restaurant/index
+    path('message', msg, name='message'), # Endpoint(needs auth): restaurant/message
+    path('api-token-auth', obtain_auth_token), # Endpoint: restaurant/api-token-auth (This is to get a token so you can access pages that need authentication)
     path('menu', menuItemsView.as_view()), # Endpoint: restaurant/menu
     path('menu/<int:pk>', singleMenuItemView.as_view()), # Endpoint: restaurant/menu/pk
+    
 ]
